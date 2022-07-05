9974fb5f0dd9c0a4a30fc31e167ad1ce61a1caf1
Andres2D <andres2d1997.1206@gmail.com>
2022-07-04 20:33:23 -0500

    Add dynamic styles to answer component

--- src/components/Answer/Answer.js
+++ src/components/Answer/Answer.js
@@ -1,10 +1,42 @@
+import { useContext } from 'react';
+import QuestionsContext from '../../store/question-context';
 import styles from './Answer.module.css';
 
 const Answer = props => {
+
+  const questionCtx = useContext(QuestionsContext);
+  const questionIdx = questionCtx.questions.findIndex(q => q.id === props.questionId); 
+  const questionObj = questionCtx.questions[questionIdx];
+  let answerStatus = null;
+
+  if(props.answer === questionObj.answerSelected) {
+    if(questionObj.answerSelected === questionObj.correct_answer) {
+      answerStatus = styles.correct;
+    }else if(questionObj.answerSelected !== questionObj.correct_answer) {
+      answerStatus = styles.incorrect;
+    }
+  }else if(questionObj.answerSelected && props.answer !== questionObj.answerSelected){
+    answerStatus = styles.disabled;
+  }else {
+    answerStatus = null;
+  }
+
+  const answerQuestionHandler = () => {
+    const answerUpdate = {
+      questionId: props.questionId,
+      answer: props.answer
+    }
+    questionCtx.updateAnswer({type: 'UPDATE', payload: answerUpdate});
+  };
+  
   return (
-    <div className={`${styles.answerCard}`}>
+    <button 
+      onClick={answerQuestionHandler} 
+      className={`${styles.answerBtn} ${answerStatus}`}
+      disabled={answerStatus === styles.disabled}
+    >
       <p className={styles.answerTitle}>{props.answer}</p>
-    </div>
+    </button>
   );
 };
 
