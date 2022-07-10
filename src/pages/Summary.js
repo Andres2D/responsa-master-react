3a42542d7102d2ae49ca2dd63636d6ce049bbc06
Andres2D <andres2d1997.1206@gmail.com>
2022-07-09 23:29:49 -0500

    Add summary page

--- src/pages/Summary.js
+++ src/pages/Summary.js
@@ -1,9 +1,39 @@
-// import styles from './Summary.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import styles from './Summary.module.css';
+import Card from "../components/UI/Card";
+import { calculateScoreMessage } from '../helpers/helpers';
+import { questionActions } from '../store/questions';
+import Button from '../components/UI/Button';
+import { useNavigate } from 'react-router-dom';
 
 const Summary = () => {
+
+  const questionsState = useSelector(state => state.questions);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const {
+    totalQuestions,
+    correctAnswers,
+    message
+  } = calculateScoreMessage(questionsState.questions);
+
+  const nextHandler =  () => {
+    dispatch(questionActions.nextQuestion());
+    navigate('/main');
+  };
+
   return (
-    <div>
-      <p>Summary works!</p>
+    <div className={styles.container}>
+      <Card className={styles.center}>
+        <h1>Score</h1>
+        <h2 className={styles.score}>{correctAnswers}/{totalQuestions}</h2>
+        <h2>{message}</h2>  
+      </Card>
+      <Button
+        title='Continue'
+        onClick={nextHandler}
+      />
     </div>
   );
 };
