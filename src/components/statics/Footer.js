f0efa5df8c40feb321dbaea6c80f13087d1597ae
Andres2D <andres2d1997.1206@gmail.com>
2022-07-10 21:54:19 -0500

    Add trivia api info in footer

--- src/components/statics/Footer.js
+++ src/components/statics/Footer.js
@@ -6,6 +6,7 @@ const Footer = () => {
       <div className={styles.container}>
         <a href="https://github.com/Andres2D/responsa-master-react">By <span className={styles.name}>Andres2D</span></a> 
         <a href="https://www.freepik.com/vectors/monkey-king">Monkey king vector created by dgim-studio - www.freepik.com</a>
+        <a href="https://opentdb.com/api_config.php">Trivia API - opentdb.com</a>
       </div>
     </footer>
   );
